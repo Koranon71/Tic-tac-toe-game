34ffa84aa5827c5a2ba92f086280c6503fe83ac0
Koranon <162946168+Koranon71@users.noreply.github.com>
2024-07-01 18:53:56 +0700

    Add files via upload

---
+++ Tic tac toe game/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace TicTacToeGame
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel mainMenuPanel;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button playWithBotButton;
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button resetButton;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.mainMenuPanel = new System.Windows.Forms.Panel();
+            this.playButton = new System.Windows.Forms.Button();
+            this.playWithBotButton = new System.Windows.Forms.Button();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.mainMenuPanel.SuspendLayout();
+            this.gamePanel.SuspendLayout();
+            this.SuspendLayout();
+
+            // mainMenuPanel
+            this.mainMenuPanel.Controls.Add(this.playButton);
+            this.mainMenuPanel.Controls.Add(this.playWithBotButton);
+            this.mainMenuPanel.Location = new System.Drawing.Point(12, 12);
+            this.mainMenuPanel.Name = "mainMenuPanel";
+            this.mainMenuPanel.Size = new System.Drawing.Size(300, 250);
+            this.mainMenuPanel.TabIndex = 0;
+
+            // playButton
+            this.playButton.Location = new System.Drawing.Point(100, 50);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(100, 50);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Играть";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+
+            // playWithBotButton
+            this.playWithBotButton.Location = new System.Drawing.Point(100, 120);
+            this.playWithBotButton.Name = "playWithBotButton";
+            this.playWithBotButton.Size = new System.Drawing.Size(100, 50);
+            this.playWithBotButton.TabIndex = 1;
+            this.playWithBotButton.Text = "Играть с ботом";
+            this.playWithBotButton.UseVisualStyleBackColor = true;
+            this.playWithBotButton.Click += new System.EventHandler(this.playWithBotButton_Click);
+
+            // gamePanel
+            this.gamePanel.Controls.Add(this.button1);
+            this.gamePanel.Controls.Add(this.button2);
+            this.gamePanel.Controls.Add(this.button3);
+            this.gamePanel.Controls.Add(this.button4);
+            this.gamePanel.Controls.Add(this.button5);
+            this.gamePanel.Controls.Add(this.button6);
+            this.gamePanel.Controls.Add(this.button7);
+            this.gamePanel.Controls.Add(this.button8);
+            this.gamePanel.Controls.Add(this.button9);
+            this.gamePanel.Controls.Add(this.resetButton);
+            this.gamePanel.Location = new System.Drawing.Point(12, 12);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(300, 300);
+            this.gamePanel.TabIndex = 1;
+            this.gamePanel.Visible = false;
+
+            // button1
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 75);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+
+            // button2
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 75);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+
+            // button3
+            this.button3.Location = new System.Drawing.Point(174, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 75);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+
+            // button4
+            this.button4.Location = new System.Drawing.Point(12, 93);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 75);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+
+            // button5
+            this.button5.Location = new System.Drawing.Point(93, 93);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 75);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+
+            // button6
+            this.button6.Location = new System.Drawing.Point(174, 93);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 75);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+
+            // button7
+            this.button7.Location = new System.Drawing.Point(12, 174);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 75);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+
+            // button8
+            this.button8.Location = new System.Drawing.Point(93, 174);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 75);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+
+            // button9
+            this.button9.Location = new System.Drawing.Point(174, 174);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 75);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_Click);
+
+            // resetButton
+            this.resetButton.Location = new System.Drawing.Point(100, 255);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(100, 30);
+            this.resetButton.TabIndex = 9;
+            this.resetButton.Text = "Играть снова";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Visible = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 321);
+            this.Controls.Add(this.gamePanel);
+            this.Controls.Add(this.mainMenuPanel);
+            this.Name = "Form1";
+            this.Text = "Крестики-нолики";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mainMenuPanel.ResumeLayout(false);
+            this.gamePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Tic tac toe game/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TicTacToeGame
+{
+    public partial class Form1 : Form
+    {
+        private bool isPlayerXTurn; // true - ход игрока Х, false - ход игрока O
+        private bool playWithBot;
+        private Random random = new Random();
+        private Color xColor;
+        private Color oColor;
+
+        public Form1()
+        {
+            InitializeComponent();
+            resetButton.Text = "Играть снова";
+            ShowMainMenu();
+        }
+
+        private void ShowMainMenu()
+        {
+            gamePanel.Visible = false;
+            resetButton.Visible = false;
+            mainMenuPanel.Visible = true;
+        }
+
+        private void StartGame(bool playWithBot)
+        {
+            this.playWithBot = playWithBot;
+            mainMenuPanel.Visible = false;
+            gamePanel.Visible = true;
+            reset();
+        }
+
+        private void EnableButtons(bool enable)
+        {
+            foreach (var button in gamePanel.Controls.OfType<Button>())
+            {
+                if (button != resetButton)
+                {
+                    button.Enabled = enable && button.Text == "";
+                }
+            }
+        }
+
+        private void reset()
+        {
+            xColor = GetRandomPastelColor();
+            oColor = GetRandomPastelColor();
+
+            foreach (var button in gamePanel.Controls.OfType<Button>())
+            {
+                if (button != resetButton)
+                {
+                    button.Text = "";
+                    button.BackColor = SystemColors.Control;
+                    button.Enabled = true;
+                }
+            }
+            isPlayerXTurn = true;
+            resetButton.Visible = false;
+        }
+
+        private void CheckGameStatus()
+        {
+            if (CheckWin("X"))
+            {
+                MessageBox.Show("Выиграл игрок X");
+                EnableButtons(false);
+                resetButton.Visible = true;
+            }
+            else if (CheckWin("O"))
+            {
+                MessageBox.Show(playWithBot ? "Выиграл компьютер" : "Выиграл игрок O");
+                EnableButtons(false);
+                resetButton.Visible = true;
+            }
+            else if (IsBoardFull())
+            {
+                MessageBox.Show("Ничья");
+                EnableButtons(false);
+                resetButton.Visible = true;
+            }
+            else if (playWithBot && !isPlayerXTurn)
+            {
+                ComputerMove();
+            }
+        }
+
+        private bool IsBoardFull()
+        {
+            return gamePanel.Controls.OfType<Button>().All(b => b.Text != "" || b == resetButton);
+        }
+
+        private bool CheckWin(string mark)
+        {
+            var buttons = gamePanel.Controls.OfType<Button>().Where(b => b != resetButton).ToArray();
+
+            int[,] winPositions = new int[,] {
+                {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Горизонтальные
+                {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Вертикальные
+                {0, 4, 8}, {2, 4, 6}             // Диагональные
+            };
+
+            for (int i = 0; i < winPositions.GetLength(0); i++)
+            {
+                if (buttons[winPositions[i, 0]].Text == mark &&
+                    buttons[winPositions[i, 1]].Text == mark &&
+                    buttons[winPositions[i, 2]].Text == mark)
+                {
+                    Color winColor = mark == "X" ? xColor : oColor;
+                    buttons[winPositions[i, 0]].BackColor = winColor;
+                    buttons[winPositions[i, 1]].BackColor = winColor;
+                    buttons[winPositions[i, 2]].BackColor = winColor;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void ComputerMove()
+        {
+            var emptyButtons = gamePanel.Controls.OfType<Button>().Where(b => b.Text == "" && b != resetButton).ToList();
+            if (emptyButtons.Count > 0)
+            {
+                int index = random.Next(emptyButtons.Count);
+                emptyButtons[index].Text = "O";
+                emptyButtons[index].Enabled = false;
+                emptyButtons[index].BackColor = oColor;
+                isPlayerXTurn = true;
+                CheckGameStatus();
+            }
+        }
+
+        private Color GetRandomPastelColor()
+        {
+            int r = (random.Next(256) + 255) / 2;
+            int g = (random.Next(256) + 255) / 2;
+            int b = (random.Next(256) + 255) / 2;
+            return Color.FromArgb(r, g, b);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (button != null && button.Text == "")
+            {
+                if (isPlayerXTurn)
+                {
+                    button.Text = "X";
+                    button.BackColor = xColor;
+                }
+                else
+                {
+                    button.Text = "O";
+                    button.BackColor = oColor;
+                }
+                button.Enabled = false;
+                isPlayerXTurn = !isPlayerXTurn;
+                CheckGameStatus();
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            StartGame(false);
+        }
+
+        private void playWithBotButton_Click(object sender, EventArgs e)
+        {
+            StartGame(true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var button in gamePanel.Controls.OfType<Button>())
+            {
+                button.Click += new EventHandler(button_Click);
+            }
+        }
+    }
+}
